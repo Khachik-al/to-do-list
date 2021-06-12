0bd0ed6acc8b4f7ab3a4ca1fd6cd3c891c78bd46
Khach <aloyanxachik9@gmail.com>
2021-06-12 18:21:39 +0400

    redux2

--- src/index.js
+++ src/index.js
@@ -2,37 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import Counter from './demo/Counter/Counter';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-
-function reducer(state = { value: 0 }, action) {
-
-
-  if (action.type === 'CHANGE_VALUE') {
-    return {
-      ...state,
-      value: state.value + 1
-    }
-  }
-  if (action.type === 'SEND_MESSAGE') {
-    return {
-      ...state,
-      message: action.message
-    }
-  }
-
-  return state;
-
-}
-
-
+import { store } from './store/store';
 
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={createStore(reducer)}>
+    <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>,
